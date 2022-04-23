1e7410582007266b7d520b572116f933f6dd54e3
changfish <100700089+changfish@users.noreply.github.com>
2022-04-24 00:14:47 +0800

    Create good.cpp

---
+++ algorithm/good.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void sort(int*,int);
+
+
+int main() {
+	int A[5]={9,8,7,6,5};
+	sort(A,5);
+	for(int i=0;i<5;i++)
+		cout<<A[i]<<"\t";
+}
+
+void sort(int *A,int K){
+	for(int i=K;i>0;i--){
+		for(int j=0;j<i;j++){
+			if(A[j+1]<A[j]){
+				A[j+1]+=A[j];
+				A[j]=A[j+1]-A[j];
+				A[j+1]-=A[j];
+			}	
+		}
+	}
+}
