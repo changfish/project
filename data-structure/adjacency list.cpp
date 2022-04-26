6c863321832785cd486fa1f6c6bab5b01968be31
changfish <100700089+changfish@users.noreply.github.com>
2022-04-26 13:23:00 +0800

    無向圖的建立-相鄰串列法
    相鄰串列法
    使用link list實作
    可用apcs2016.3.16第四題的測資
    https://zerojudge.tw/ShowProblem?problemid=b967
    8
    0 1
    0 2
    0 3
    7 0
    1 4
    1 5
    3 6
    複雜度還是太大=P

---
+++ data-structure/adjacency list.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+
+class list{
+	public:
+		int val;
+		class list *next;
+};
+typedef class list node;
+typedef node *link;
+
+void add_node(link,int); 
+
+int main()
+{	
+	int n;
+	cin>>n;	//成員總數，所以需要有n-1行來儲存成員個數
+	int A[n][2];
+	link h[n],ptr;
+	for(int i=0;i<n-1;i++){
+		cin>>A[i][0]>>A[i][1];
+		h[i]=new node;
+		h[i]->next=NULL;
+		h[i]->val=i;		
+	}
+	h[n-1]=new node;		//輸入時沒建立第n個串列首 所以在這邊建立 
+	h[n-1]->next=NULL;
+	h[n-1]->val=n-1;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-1;j++){
+			if(A[j][0]==h[i]->val)
+				add_node(h[i],A[j][1]);
+				add_node(h[A[j][1]],A[j][0]);
+		}
+	}
+	for(int i=0;i<n;i++){
+		ptr=h[i];
+		while(ptr!=NULL){
+			cout<<ptr->val<<" ";
+			ptr=ptr->next;	
+		}
+		cout<<endl;
+	}
+}
+
+void add_node(link h,int val){
+	link ptr,newnode;
+	ptr=h;
+	newnode =new node;
+	newnode->next=NULL;
+	newnode->val=val;
+	if(ptr->next!=NULL){
+		int s=0;		//判斷是否有重複的元素 
+		while(ptr->next!=NULL){
+			ptr=ptr->next;
+			if(ptr->val==val){
+				s++;
+				break;
+			}
+		}
+		if(!s)
+			ptr->next=newnode;
+	}else{
+		if(!(ptr->val==val))
+			ptr->next=newnode;
+	}
+}
+
+
+
+
+
