02ba14b6217d901ddc68a8390a9093d2ba3e2ec3
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-17 19:40:18 +0800

    Update l2cachesim.h

--- Ripes/l2cachesim.h
+++ Ripes/l2cachesim.h
@@ -1,19 +1,19 @@
-#include "cacheinterface.h"
+#pragma once
+
+#include <QObject>
+#include "cachesim.h"
 
 namespace Ripes {
 
 class L2CacheSim : public CacheSim {
   Q_OBJECT
 public:
-  explicit L2CacheSim(QObject *parent = nullptr);
+  L2CacheSim(QObject *parent = nullptr);
+  
   void access(AInt address, MemoryAccess::Type type) override;
-  void reset() override;
-  void reverse() override;
 
-  void setPreset(const CachePreset &preset) override;
-
-private:
-  void analyzeCacheAccess(CacheTransaction &transaction) const override;
+signals:
+  void configurationChanged();
 };
 
 } // namespace Ripes
