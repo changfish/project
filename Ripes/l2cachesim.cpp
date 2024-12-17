64c80284df1f188f31ff0661bc3ec1dbf25920e5
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-17 22:02:11 +0800

    Update l2cachesim.cpp

--- Ripes/l2cachesim.cpp
+++ Ripes/l2cachesim.cpp
@@ -2,12 +2,26 @@
 
 namespace Ripes {
 
-L2CacheSim::L2CacheSim(QObject *parent)
-    : CacheSim(parent) {}
+L2CacheSim::L2CacheSim(QObject *parent) : CacheInterface(parent) {}
 
 void L2CacheSim::access(AInt address, MemoryAccess::Type type) {
-  CacheTransaction transaction{address, getCacheIndex(address), false, false, type, false, false};
-  analyzeCacheAccess(transaction);
+  if (isHit(address)) {
+    emit cacheEvent(CacheEvent::Hit, address);
+  } else {
+    emit cacheEvent(CacheEvent::Miss, address);
+    insertAddress(address);
+  }
+}
+
+bool L2CacheSim::isHit(AInt address) {
+  return m_cacheLines.find(address) != m_cacheLines.end();
+}
+
+void L2CacheSim::insertAddress(AInt address) {
+  if (m_cacheLines.size() >= L2_SIZE) {
+    m_cacheLines.erase(m_cacheLines.begin()); // Simple eviction policy (FIFO)
+  }
+  m_cacheLines.insert(address);
 }
 
 } // namespace Ripes
