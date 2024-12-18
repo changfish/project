94b42bced698c0cf1b6b1d36705019db7cb3f2a9
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-18 10:37:06 +0800

    Update l2cache.h

--- Ripes/l2cache.h
+++ Ripes/l2cache.h
@@ -1,22 +1,29 @@
 #pragma once
 
-#include "cachesim.h"
+#include <unordered_map>
+#include <memory>
+#include "VSRTL/core/vsrtl_memory.h"
+#include "isa/isa_types.h"
 
 namespace Ripes {
 
 /**
  * @brief The L2Cache class
- * Implements a simple L2 cache which acts as the next level cache in the hierarchy.
+ * Represents the L2 cache in the cache hierarchy.
  */
-class L2Cache : public CacheInterface {
-  Q_OBJECT
+class L2Cache {
 public:
-  L2Cache(QObject *parent = nullptr);
-  void access(AInt address, MemoryAccess::Type type) override;
+  L2Cache();
+  ~L2Cache();
+
+  bool isHit(AInt address);
+  void access(AInt address, MemoryAccess::Type type);
+  void reset();
+  void reverse();
 
 private:
-  bool isHit(AInt address);
+  // Cache structure and management details
+  std::unordered_map<AInt, MemoryAccess::Type> cacheEntries;
 };
 
 } // namespace Ripes
- 
