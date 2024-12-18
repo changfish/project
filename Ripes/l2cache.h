e1c02780816c756b0cb6e903cbbeca7d495678e0
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-18 10:14:08 +0800

    Create l2cache.h

---
+++ Ripes/l2cache.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "cachesim.h"
+
+namespace Ripes {
+
+/**
+ * @brief The L2Cache class
+ * Implements a simple L2 cache which acts as the next level cache in the hierarchy.
+ */
+class L2Cache : public CacheInterface {
+  Q_OBJECT
+public:
+  L2Cache(QObject *parent = nullptr);
+  void access(AInt address, MemoryAccess::Type type) override;
+
+private:
+  bool isHit(AInt address);
+};
+
+} // namespace Ripes
