039679a895bc6b3c4b23c86a4346f4f67b88fe84
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-18 12:44:06 +0800

    Update l2cacheshim.h

--- Ripes/l2cacheshim.h
+++ Ripes/l2cacheshim.h
@@ -8,6 +8,10 @@ class L2CacheShim : public CacheSim {
   Q_OBJECT
 public:
   L2CacheShim(QObject *parent = nullptr) : CacheSim(parent) {}
+
+  void access(AInt address, MemoryAccess::Type type) override {
+    CacheSim::access(address, type);
+  }
 };
 
 } // namespace Ripes
