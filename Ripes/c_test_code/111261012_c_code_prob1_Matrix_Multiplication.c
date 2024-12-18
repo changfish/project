5d8143953955ede043bd09dc3265d41e944b6a1e
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-18 13:18:49 +0800

    Create 111261012_c_code_prob1_Matrix_Multiplication.c

---
+++ Ripes/c_test_code/111261012_c_code_prob1_Matrix_Multiplication.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define N 8
+
+int A[N][N], B[N][N], C[N][N];
+
+void matrix_multiply() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = 1;
+            B[i][j] = 1;
+        }
+    }
+
+    matrix_multiply();
+    return 0;
+}
