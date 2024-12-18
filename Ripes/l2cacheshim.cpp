f468646b2791ff9b91444e38560c3066fc4a5c34
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-18 12:44:33 +0800

    Update l2cacheshim.cpp

--- Ripes/l2cacheshim.cpp
+++ Ripes/l2cacheshim.cpp
@@ -3,7 +3,9 @@
 namespace Ripes {
 
 void L2CacheShim::access(AInt address, MemoryAccess::Type type) {
-  CacheSim::access(address, type); // 使用已有的 CacheSim 逻辑处理 L2 缓存访问
+  CacheSim::access(address, type);
 }
 
 } // namespace Ripes
+
+// l1cacheshim.h
