715ce555b9ebdd074feb6d5b1939a30e0d416553
chang yu <100700089+changfish@users.noreply.github.com>
2024-12-18 12:45:45 +0800

    Update l1cacheshim.cpp

--- Ripes/l1cacheshim.cpp
+++ Ripes/l1cacheshim.cpp
@@ -4,33 +4,32 @@
 namespace Ripes {
 
 L1CacheShim::L1CacheShim(CacheType type, QObject *parent)
-    : CacheInterface(parent), m_type(type), m_l2Cache(std::make_unique<L2Cache>()) {
-  connect(ProcessorHandler::get(), &ProcessorHandler::processorReset, this, &L1CacheShim::processorReset);
-  connect(ProcessorHandler::get(), &ProcessorHandler::processorClocked, this, &L1CacheShim::processorWasClocked, Qt::DirectConnection);
-  connect(ProcessorHandler::get(), &ProcessorHandler::processorReversed, this, &L1CacheShim::processorReversed);
+    : CacheInterface(parent), m_type(type) {
+  connect(ProcessorHandler::get(), &ProcessorHandler::processorReset, this,
+          &L1CacheShim::processorReset);
+
+  connect(ProcessorHandler::get(), &ProcessorHandler::processorClocked, this,
+          &L1CacheShim::processorWasClocked, Qt::DirectConnection);
+  connect(ProcessorHandler::get(), &ProcessorHandler::processorReversed, this,
+          &L1CacheShim::processorReversed);
 
   processorReset();
 }
 
-void L1CacheShim::access(AInt address, MemoryAccess::Type type) {
-  if (!m_l2Cache->isHit(address)) {
-    updateStats(false); // L1 miss, go to L2
-    m_l2Cache->access(address, type); // L2 access
-    updateStats(true);  // L1 hit after L2 access
-  } else {
-    updateStats(true); // L1 hit
-  }
+void L1CacheShim::access(AInt, MemoryAccess::Type) {
+  Q_ASSERT(false);
 }
 
 void L1CacheShim::processorReset() {
   CacheInterface::reset();
-  m_l2Cache->reset(); // Reset the L2 cache
-  processorWasClocked();
+
+  if (m_nextLevelCache) {
+    processorWasClocked();
+  }
 }
 
 void L1CacheShim::processorReversed() {
   CacheInterface::reverse();
-  m_l2Cache->reverse(); // Reverse operation for L2 cache
 }
 
 void L1CacheShim::processorWasClocked() {
@@ -39,7 +38,6 @@ void L1CacheShim::processorWasClocked() {
 
     if (dataAccess.type == MemoryAccess::Write || dataAccess.type == MemoryAccess::Read) {
       if (!m_nextLevelCache->access(dataAccess.address, dataAccess.type)) {
-        // 如果在 L1 缓存未命中，则继续访问 L2 缓存
         if (m_l2Cache) {
           m_l2Cache->access(dataAccess.address, dataAccess.type);
         }
@@ -56,6 +54,5 @@ void L1CacheShim::processorWasClocked() {
     }
   }
 }
-
 
 } // namespace Ripes
